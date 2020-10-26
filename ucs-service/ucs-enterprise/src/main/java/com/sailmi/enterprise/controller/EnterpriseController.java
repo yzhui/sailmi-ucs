7a33ca9114920e4d1e3494d540d42117adb7c07e
suyt <suyt@linose.com>
2020-10-26 14:41:01 +0800

    企业入驻本地调试无误

--- ucs-service/ucs-enterprise/src/main/java/com/sailmi/enterprise/controller/EnterpriseController.java
+++ ucs-service/ucs-enterprise/src/main/java/com/sailmi/enterprise/controller/EnterpriseController.java
@@ -253,15 +253,15 @@ public class EnterpriseController extends AppController {
 	@ApiOperationSupport(order = 8)
 	@ApiOperation(value = "创建企业", notes = "传入企业信息,详细信息,以及财务信息")
 	public R create(AuthUser authUser, /*@Valid @RequestBody*/ Enterprise enterprise, EnterpriseDetails enterpriseDetails, EnterpriseFinance enterpriseFinance){
-//		enterprise.setTenantId(authUser.getTenantId());//设置租户ID
-		enterprise.setTenantId("123321");//设置租户ID
+		enterprise.setTenantId(authUser.getTenantId());//设置租户ID
+//		enterprise.setTenantId("123321");//设置租户ID
 		enterpriseService.saveEnterpriseInfo(enterprise); //首先插入企业基本信息
 		Long id = enterprise.getId();
 		//企业基本信息插入后插入企业详细信息
 		if (id != 0 && id != null) {//企业基本信息插入成功后插入企业详细信息
 			enterpriseDetails.setEnterpriseId(id);//企业详细信息关联企业ID
-			EnterpriseDetails saveDetail = iEnterpriseDetailsService.saveDetail(enterpriseDetails);
-			Long deId = saveDetail.getId();
+			iEnterpriseDetailsService.saveDetail(enterpriseDetails);
+			Long deId = enterpriseDetails.getId();
 			if (deId != 0 && deId != null) {//详细信息插入成功后插入财务信息
 				enterpriseFinance.setEnterpriseId(id);
 				int finace = iEnterpriseFinanceService.saveFiance(enterpriseFinance);
