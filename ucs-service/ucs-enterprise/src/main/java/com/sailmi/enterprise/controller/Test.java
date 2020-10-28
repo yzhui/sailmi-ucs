fe4dceab8be19eb275565f3930bf33c97f07034d
xingsg <xingsg@linose.com>
2020-10-28 13:38:37 +0800

    feign

--- ucs-service/ucs-enterprise/src/main/java/com/sailmi/enterprise/controller/Test.java
+++
@@ -1,9 +0,0 @@
-package com.sailmi.enterprise.controller;
-
-import com.sailmi.core.tool.utils.DigestUtil;
-
-public class Test {
-	public static void main(String[] args) {
-		DigestUtil.encrypt("123");
-	}
-}
