56b18ce0b046f41114bd0a1cc5252b8d6c6a8750
xingsg <xingsg@linose.com>
2020-11-03 09:35:58 +0800

    去掉tenantId

--- ucs-auth/src/main/java/com/sailmi/auth/controller/AuthController.java
+++ ucs-auth/src/main/java/com/sailmi/auth/controller/AuthController.java
@@ -72,7 +72,7 @@ public class AuthController {
 	@ApiOperation(value = "获取认证token", notes = "传入租户ID:tenantId,账号:account,密码:password")
 	public R token(@ApiParam(value = "授权类型", required = true) @RequestParam(defaultValue = "password", required = false) String grantType,
 							 @ApiParam(value = "刷新令牌") @RequestParam(required = false) String refreshToken,
-							 @ApiParam(value = "租户ID", required = true) @RequestParam(defaultValue = "000000", required = false) String tenantId,
+							 @ApiParam(value = "租户ID", required = false) @RequestParam(defaultValue = "000000", required = false) String tenantId,
 							 @ApiParam(value = "账号") @RequestParam(required = false) String account,
 							 @ApiParam(value = "密码") @RequestParam(required = false) String password) {
 
