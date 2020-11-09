87afb853f82c851720ed98a16d8a1a30692b5cbf
xingsg <xingsg@linose.com>
2020-11-09 14:05:14 +0800

    测试企业数据完成

--- ucs-service/ucs-system/src/main/java/com/sailmi/system/feign/TenantLoginColle.java
+++ ucs-service/ucs-system/src/main/java/com/sailmi/system/feign/TenantLoginColle.java
@@ -6,6 +6,7 @@ import com.sailmi.system.entity.Tenant;
 import com.sailmi.system.service.ITenantService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class TenantLoginColle implements  TenantFeign{
 
 	@Override
 	@PostMapping(API_PREFIX + "/tenatinfo-list")
-	public R<List<Tenant>> queryLoginUserTeants(String enterpriseId) {
+	public R<List<Tenant>> queryLoginUserTeants(@RequestParam(value = "enterpriseId",required = true) String enterpriseId) {
 		QueryWrapper<Tenant> tenantQueryWrapper = new QueryWrapper<>();
 		tenantQueryWrapper.eq("enterprise_id",enterpriseId);
 		List<Tenant> list = tenantService.list(tenantQueryWrapper);
