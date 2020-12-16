c3a84364e7f0a4d6e4860df8b59d43e0a4877696
suyt <suyt@linose.com>
2020-12-16 13:47:21 +0800

    内容管理的基本代码漏掉的

---
+++ ucs-service/ucs-cms/src/main/java/com/sailmi/cms/service/ITopicContentService.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2018-2028, Chill Zhuang 庄骞 (smallchill@163.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sailmi.cms.service;
+
+import com.sailmi.cms.entity.TopicContent;
+import com.sailmi.cms.vo.TopicContentVO;
+import com.sailmi.core.mp.base.BaseService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+
+/**
+ * 专题内容关系 服务类
+ *
+ * @author sailmi
+ * @since 2020-12-16
+ */
+public interface ITopicContentService extends BaseService<TopicContent> {
+
+	/**
+	 * 自定义分页
+	 *
+	 * @param page
+	 * @param topicContent
+	 * @return
+	 */
+	IPage<TopicContentVO> selectTopicContentPage(IPage<TopicContentVO> page, TopicContentVO topicContent);
+
+}
