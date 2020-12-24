77f016900037f9e0ca1bfb09a540400041511c4c
suyt <suyt@linose.com>
2020-12-24 14:45:08 +0800

    cms树工具类的bug

--- ucs-service/ucs-cms/src/main/java/com/sailmi/cms/utils/TreeUtils.java
+++ ucs-service/ucs-cms/src/main/java/com/sailmi/cms/utils/TreeUtils.java
@@ -6,25 +6,6 @@ import java.util.List;
 
 public class TreeUtils{
 
-
-	public static List<TreeEntity> listTotree(List<TreeEntity> list) {
-        List<TreeEntity> treeList = new ArrayList<TreeEntity>();
-        for (TreeEntity tree : list) {
-            for (TreeEntity TreeEntity : list) {
-                if (TreeEntity.getPid().compareTo(tree.getId())==0) {
-                    if (tree.getChildren()==null) {
-                        tree.setChildren(new ArrayList<TreeEntity>());
-                    }
-                    tree.getChildren().add(TreeEntity);
-                }
-            }
-        }
-        return treeList;
-    }
-
-
-
-
 	//菜单树形结构
 	public static List<TreeEntity> treeMenuList(List<TreeEntity> treeList, Long parentId) {
 		ArrayList<TreeEntity> childList = new ArrayList<TreeEntity>();
